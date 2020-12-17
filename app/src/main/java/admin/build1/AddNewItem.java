c26182b28389c16c22a695d2ceaf3baf0f3a3e56
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2020-12-17 22:32:46 +0300

    New Form
    Form for creating new Hotel

--- app/src/main/java/admin/build1/AddNewItem.java
+++ app/src/main/java/admin/build1/AddNewItem.java
@@ -16,27 +16,47 @@ public class AddNewItem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
 
-        TextView name = (TextView)findViewById(R.id.newItemName);
-        name.setText(getIntent().getStringExtra("testValue"));
     }
 
 
     public void addItem(View view) {
 
-        TextView name = (TextView)findViewById(R.id.newItemName);
+        TextView nameView = (TextView)findViewById(R.id.newItemName);
+        String name = nameView.getText().toString();
 
-        switch (getIntent().getStringExtra("itemType")){
+        TextView contactView = (TextView) findViewById(R.id.newItemContacts);
+        String contact = contactView.getText().toString();
+
+        String itemType = getIntent().getStringExtra("itemType");
+
+        TraveliaDatabaseHelper databaseService = TraveliaDatabaseHelper.getInstance(this);
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase(TraveliaDatabaseHelper.DB_NAME, MODE_PRIVATE, null);
+
+        int kittyImageId = R.drawable.kitty;
+
+        switch (itemType) {
             case "hotel":
             {
-                TraveliaDatabaseHelper databaseService = TraveliaDatabaseHelper.getInstance(this);
-                SQLiteDatabase db = getBaseContext().openOrCreateDatabase(TraveliaDatabaseHelper.DB_NAME, MODE_PRIVATE, null);
-                databaseService.insertHotel(db, name.getText().toString(), "",0,0);
-                ToastService.showToast(this, "Record was created?");
-
+                databaseService.insertHotel(db, name, contact,kittyImageId,kittyImageId);
+                break;
+            }
+            case "sight": {
+                databaseService.insertSight(db, name, "","", contact, "", 0, 0, kittyImageId);
+                break;
+            }
+            case "coffee": {
+                databaseService.insertCafe(db, name, contact, kittyImageId, kittyImageId);
+                break;
+            }
+            case "taxi": {
+                databaseService.insertTaxi(db, name, "111", "111", contact, kittyImageId);
+                break;
+            }
+            case "park": {
+                databaseService.insertPark(db, name, "park is awesome", 0, 0, kittyImageId);
                 break;
             }
         }
-
 
     }
 }
